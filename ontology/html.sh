e4d1eb2125e5b192ae10e0c00140696aa0d79614
Esmé Cowles <escowles@ucsd.edu>
2015-02-20 17:29:19 -0500

    Renaming ontology from dams5 to dams4.2

--- ontology/html.sh
+++ ontology/html.sh
@@ -3,8 +3,8 @@
 echo "dams.owl -> dams.html"
 xsltproc owl2html.xsl dams.owl > dams.html
 
-echo "dams5.rdf -> dams5.html"
-xsltproc rdfs2html.xsl dams5.rdf > dams5.html
+echo "dams4.2.rdf -> dams4.2.html"
+xsltproc rdfs2html.xsl dams4.2.rdf > dams4.2.html
 
 echo "works.rdf -> works.html"
 xsltproc rdfs2html.xsl works.rdf > works.html
